2ea7d86573ae47e26858eb858a71f3f40b6b80ad
Magali179 <malali.moreno@gmail.com>
2026-05-09 16:57:31 -0300

    entrega TP_2_VUEJS

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,12 +1,12 @@
 <template>
     <h3>Lista de Productos</h3>
 
-    <div v-if="state.lista.length === 0">
+    <div v-if="props.productos.length === 0">
         <p>No hay productos disponibles.</p>
     </div>
     <ul>
         <ProductItem 
-            v-for="producto in state.lista" 
+            v-for="producto in props.productos" 
             :key="producto.id" 
             :producto="producto" 
            
@@ -20,15 +20,11 @@ import {reactive, watch} from 'vue'
 import ProductItem from './ProductItem.vue'
 
 const props = defineProps({
-    lista: Array,
+    productos: Array,
 })
 const emit = defineEmits(['eliminar'])
-const state = reactive({
-    lista: props.lista
-})
-watch(() => props.lista, (newLista) => {
-    state.lista = newLista
-})
+
+
 function eliminarProducto(id) {
     emit('eliminar', id)
 }
